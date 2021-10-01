107165b622a6f63cf242e29eacdf34c643909a7d
Sergey Kovylov <sere99ka@gmail.com>
2021-10-01 11:23:02 +0300

    Выполнил полностью задание

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('работает');
+let num = 266219;
+let numStr = String(num)
+let result = 1;
+
+for (i = 0; i < numStr.length; i++) {
+    result = result * Number(numStr[i])
+}
+
+console.log(result);
+
+resultDeg = result ** 3
+resultDegStr = String(resultDeg)
+
+console.log(resultDegStr.substr(0, 2));
