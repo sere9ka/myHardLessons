6c95bcd5dd50bd214cd3fe815231c9821629b359
sere9ka <63472789+sere9ka@users.noreply.github.com>
2021-10-06 09:41:22 +0300

    Выполнил усложненное

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('работает');
+let str = prompt("Введите любую строку!", " Введите любую строку! ")
+
+const getStrClip = function (string) {
+    if (string === null) {
+        return "Вы ничего не ввели!"
+    } else if (string == '') {
+        return "Вы ввели не строку, а непонятно что!"
+    } else if (string == string*1) {
+        return "Вы ввели не строку, а число!"
+    } else {
+        let stringOneSpace = str.trim();
+        if (stringOneSpace.length > 29) {
+            let stringShort = stringOneSpace.slice(0, 29) + "...";
+            return stringShort;
+        } else {
+            return stringOneSpace;
+        }
+    }
+}
+
+
+console.log(getStrClip(str));
