8705c4391dd1cc0ad10533b65b728ca58caabbf6
sere9ka <63472789+sere9ka@users.noreply.github.com>
2021-10-09 17:41:53 +0300

    исправил индекс, добавил условия

--- script.js
+++ script.js
@@ -2,16 +2,31 @@
 let week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
 let allDays = document.querySelector('.days');
 let momentDay = new Date();
-let day;
+let day = momentDay.getDay() - 1;
 
 const getDate = function () {
     
-    week.forEach(function (item) {
+    week.forEach(function (item, i) {
         let newDiv = document.createElement('div');
-        newDiv.classList.add('day-item');
-        newDiv.innerHTML = item;
-        allDays.appendChild(newDiv)
+        if (i == 5 && i == day || i == 6 && i == day) {
+            newDiv.classList.add('day-item', 'italic', 'bold');
+            newDiv.innerHTML = item;
+            allDays.appendChild(newDiv)
+        } else if (i == 5 || i == 6) {
+            newDiv.classList.add('day-item', 'italic');
+            newDiv.innerHTML = item;
+            allDays.appendChild(newDiv)
+        } else if (i == day) {
+            newDiv.classList.add('day-item', 'bold');
+            newDiv.innerHTML = item;
+            allDays.appendChild(newDiv)
+        } else {
+            newDiv.classList.add('day-item');
+            newDiv.innerHTML = item;
+            allDays.appendChild(newDiv)
+        }
     });
     
 }
 getDate();
+console.log(day);
