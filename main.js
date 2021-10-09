db0916508841c80914a1e91beb7cdead9cfb0d01
sere9ka <63472789+sere9ka@users.noreply.github.com>
2021-10-09 18:29:33 +0300

    добавил первое задание

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 let num = 100;
+let arr = ['545452', '426548', '545654', '546542', '245268', '246542', '548569']
 
+for (let i = 0; i < arr.length; i++) {
+    let arrI = arr[i].split('')
+    if (arrI[0] == 2 || arrI[0] == 4) {
+        console.log(arrI);
+    }
+}
 
 nextPtime:
 for (let i = 2; i <= num; i++) {
